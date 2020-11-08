b518c39b6cdf9a0ae45141683070febbc207ddb2
lowfc <lowfc@mail.ru>
2020-11-08 23:08:44 +0500

    master

--- notifProcessor.js
+++ notifProcessor.js
@@ -1,14 +1,21 @@
-let myblock = document. querySelectorAll('.errBlock');
-let myblock2 = document. querySelectorAll('.succBlock');
-myblock.forEach(block => block. addEventListener('click', removeBlock));
-myblock2.forEach(block => block. addEventListener('click', removeBlock));
-function removeBlock() {
-    let block = this;
-    block.style.opacity = 1;
-    let blocked = setInterval(function() {
-        if (block.style.opacity > 0) block.style.opacity -= .1;
-        else {
-            block.remove();
+setTimeout(
+    () => {
+        let myblock = document. querySelectorAll('.errBlock');
+        let myblock2 = document. querySelectorAll('.succBlock');
+        myblock2.forEach(block => removeBlock(block));
+        myblock.forEach(block => removeBlock(block));
+        function removeBlock(blocker) {
+            let block = blocker;
+            block.style.opacity = 1;
+            let blocked = setInterval(function() {
+                if (block.style.opacity > 0) block.style.opacity -= .1;
+                else {
+                    block.remove();
+                }
+            }, 60)    
         }
-    }, 60)
-}
+    },
+    2000
+  );
+ 
+    
