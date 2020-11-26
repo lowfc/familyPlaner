3d7a490dda0ee5fd4c8b6f5ffbb2b3776b93fe01
lowfc <lowfc@mail.ru>
2020-11-27 00:06:31 +0500

    minor bug fixed

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" href="css/style.css">
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet">
 <div class='header'>
-    <a href="/lk.php" class='logo_a'><img class='header_logo' src='res/logo.png'></a>
+    <a href="/calculator.php" class='logo_a'><img class='header_logo' src='res/logo.png'></a>
     <a href="/expenditure_category.php" class='head_a'>Категории расходов</a>
     <a href="/revenue.php" class='head_a_2'>Доходы</a>
     <a href="/expenses.php" class='head_a_1'>Расходы</a>
